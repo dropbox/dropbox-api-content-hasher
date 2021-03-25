f7fb98fc80391360f3bdcec79cf065ae7b76862b
ok-man-com <81354505+ok-man-com@users.noreply.github.com>
2021-03-25 11:57:52 +0000

    Create DropBoxCheck.sh

---
+++ bash/DropBoxCheck.sh
@@ -0,0 +1,16 @@
+if [ -f "$1" ]; then
+   BlockCount=$((($(stat -c %s "$1") + 4194303) / 4194304))
+   AllSha=""
+   ThisBlock=0
+   while [ $ThisBlock -lt $BlockCount ]; do
+      ThisSHA=$(dd if="$1" bs=4194304 count=1 skip=$ThisBlock 2>/dev/null|sha256sum)
+      for Each in {0..62..2}; do
+         AllSha+="\x${ThisSHA:$Each:2}"
+      done
+      ((ThisBlock++))
+   done
+   Result=$(printf "$AllSha"|sha256sum)
+   echo ${Result%% *}
+else
+   echo "Error.  '$1' does not appear to be a file."
+fi
